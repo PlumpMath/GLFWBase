0c6a2f24ebb52086800685e06c715dc582e6f82a
Erik Svedäng <erik.svedang@gmail.com>
2015-05-02 13:32:39 +0200

    Initial Commit

---
+++ GLFWBase/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  GLFWBase
+//
+//  Created by Erik Svedäng on 02/05/15.
+//  Copyright (c) 2015 Erik Svedäng. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
